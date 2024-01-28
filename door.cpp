53aba5e0c7f0f326a7e6a7b6d981e40a2baf3bd4
Mr_OIG <oneebishfaq9@gmail.com>
2024-01-28 19:56:54 +0500

    file created

---
+++ door.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Door{
+private:
+bool isopen;
+bool isclose;
+public:
+Door();
+void opendoor();
+void closedoor();
+void lockdoor();
+void unlockdoor();
+bool isdooropen();
+bool isdoorclose();
+}
+int main(){
+    return 0;
+}
